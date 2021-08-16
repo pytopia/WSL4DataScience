e90050d59e7e2303628750e14f92d2a88a32264b
Sina Karaji <82142947+siniorone@users.noreply.github.com>
2021-08-16 23:40:06 +0430

    Update WSL-Beautifier.sh

--- WSL-Beautifier.sh
+++ WSL-Beautifier.sh
@@ -135,7 +135,6 @@ rm -r -f /mnt/c/myfonts
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
 /bin/echo -e "\e[1;33m   !      Installing Oh My ZSH ...Proceeding     !\e[0m"
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
-exit
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #---------------------------------------------------#
 #                  Finish
