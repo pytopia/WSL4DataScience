dd95b84002d8ff3427072a11f154d6665b03308c
Sina Karaji <82142947+siniorone@users.noreply.github.com>
2021-08-19 02:34:22 +0430

    Update WSL-Beautifier.sh

--- WSL-Beautifier.sh
+++ WSL-Beautifier.sh
@@ -12,6 +12,7 @@
 #              You can use this script and distribute it as long as credits are kept 
 #              in place and unchanged   
 # runnig script :$ sudo bash -c  "$(wget -qO- https://git.io/J0Tr6)"
+# fixing zshrc  :$ sudo bash  ~/.fixzsh.sh  
 ####################################################################################################
 #---------------------------------------------------#
 # Section 1  - Welcome & UPDATE
@@ -27,50 +28,51 @@ echo $SUDO_USER
 /bin/echo -e "\e[1;36m   !          Credits : Written by Siniorone - Aug. 2021            !\e[0m"
 /bin/echo -e "\e[1;36m   !          https://github.com/siniorone/WSL-Beautifier           !\e[0m"
 /bin/echo -e "\e[1;36m   !----------------------------------------------------------------!\e[0m"
-apt update -y
-apt upgrade -y
+sudo apt update
+sudo apt upgrade
 #---------------------------------------------------#
 # Section 2  - ZSH installation....
 #---------------------------------------------------#
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
 /bin/echo -e "\e[1;33m   !   Installing ZSH Packages...Proceeding...   !\e[0m"
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
-apt install -y zsh
+sudo apt install zsh
+sudo mv -f  /root/.zsh /home/$SUDO_USER/.zsh
 #---------------------------------------------------#
 # Section 3  - Powerline  installation....
 #---------------------------------------------------#
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
 /bin/echo -e "\e[1;33m   !   Installing Powerline Pkgs...Proceeding    !\e[0m"
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
-apt install -y powerline fonts-powerline
+sudo apt install powerline fonts-powerline
 #---------------------------------------------------#
 # Section 4  - Powerlevel9k  installation....
 #---------------------------------------------------#
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
 /bin/echo -e "\e[1;33m   !   Installing powerlevel9k ...Proceeding     !\e[0m"
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
-apt install -y zsh-theme-powerlevel9k
+sudo apt install zsh-theme-powerlevel9k
 #---------------------------------------------------#
 # Section 5  - Syntax Highlighting  installation....
 #---------------------------------------------------#
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
 /bin/echo -e "\e[1;33m   ! Installing Syntax Highlighting...Proceeding !\e[0m"
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
-apt install -y zsh-syntax-highlighting
+sudo apt install zsh-syntax-highlighting
 #---------------------------------------------------#
 # Section 6  - Git  installation....
 #---------------------------------------------------#
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
 /bin/echo -e "\e[1;33m   !        Installing Git ...Proceeding         !\e[0m"
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
-apt install git
+sudo apt install git
 #---------------------------------------------------#
 # Section 7  - Autosuggestions  installation....
 #---------------------------------------------------#
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
 /bin/echo -e "\e[1;33m   !   Installing Autosuggestions ...Proceeding  !\e[0m"
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/$SUDO_USER/.zsh/zsh-autosuggestions
 #---------------------------------------------------#
 # Section 8  - zshrc  config and conda initialization
 #---------------------------------------------------#
@@ -92,8 +94,25 @@ bindkey '^ ' autosuggest-accept
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(anaconda user dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history ram ssh)
 EOF
-
-
+source /home/$SUDO_USER/anaconda3/etc/profile.d/conda.sh
+conda init zsh
+sudo cp ~/.zshrc /home/$SUDO_USER/.fixmyzsh
+cat << EOF > /home/$SUDO_USER/.fixzsh.sh
+cat /home/$SUDO_USER/.fixmyzsh > /home/$SUDO_USER/.zshrc
+cat /home/$SUDO_USER/.fixmyzsh > /root/.zshrc
+sudo usermod -s /usr/bin/zsh $SUDO_USER
+sudo mv -f  /root/.oh-my-zsh /home/$SUDO_USER/.oh-my-zsh
+/bin/echo -e "\e[1;36m   !----------------------------------------------------------------!\e[0m"
+/bin/echo -e "\e[1;36m   !  Installation Completed...Please test your ZSH configuration   !\e[0m" 
+/bin/echo -e "\e[1;36m   !          now close your terminal and open it again             !\e[0m"
+/bin/echo -e "\e[1;36m   !      and change defult font from Properties>Fonts to the       !\e[0m"
+/bin/echo -e "\e[1;36m                 [ DejaVu Sans Mono Bold for Powerline ]            !\e[0m"
+/bin/echo -e "\e[1;36m   !                                                                !\e[0m"
+/bin/echo -e "\e[1;36m   !          Credits : Written by Siniorone - Aug. 2021            !\e[0m"
+/bin/echo -e "\e[1;36m   !          https://github.com/siniorone/WSL-Beautifier           !\e[0m"
+/bin/echo -e "\e[1;36m   !----------------------------------------------------------------!\e[0m"
+EOF
+sudo cp /home/$SUDO_USER/.fixzsh.sh ~/.fixzsh.sh
 #---------------------------------------------------#
 # Section 9  - Fonts and Color  installation....
 #---------------------------------------------------#
@@ -105,12 +124,12 @@ mkdir /mnt/c/myfonts
 cd /mnt/c/myfonts
 git clone https://github.com/powerline/fonts.git
 cd fonts
-cp /mnt/c/myfonts/fonts/install.ps1 /mnt/c/myfonts/install.ps1
+sudo cp /mnt/c/myfonts/fonts/install.ps1 /mnt/c/myfonts/install.ps1
 find ./ -mindepth 1 ! -regex '^./DejaVuSansMono\(/.*\)?' -delete
-cp /mnt/c/myfonts/install.ps1 /mnt/c/myfonts/fonts/install.ps1
-/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe C:/myfonts/fonts/install.ps1
-/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Set-ExecutionPolicy -Scope Process -ExecutionPolicy Default
+sudo cp /mnt/c/myfonts/install.ps1 /mnt/c/myfonts/fonts/install.ps1
+sudo /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+sudo /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe C:/myfonts/fonts/install.ps1
+sudo /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Set-ExecutionPolicy -Scope Process -ExecutionPolicy Default
 cd ~
 rm -r -f /mnt/c/myfonts
 #---------------------------------------------------#
@@ -119,20 +138,11 @@ rm -r -f /mnt/c/myfonts
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
 /bin/echo -e "\e[1;33m   !      Installing Oh My ZSH ...Proceeding     !\e[0m"
 /bin/echo -e "\e[1;33m   !---------------------------------------------!\e[0m"
+sudo rm -r -f /home/$SUDO_USER/.zshrc.pre-oh-my-zsh
+sudo rm -r -f /home/$SUDO_USER/.oh-my-zsh
+sudo rm -r -f ~/.zshrc.pre-oh-my-zsh
 sudo rm -r -f ~/.oh-my-zsh
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #---------------------------------------------------#
 #                  Finish
 #---------------------------------------------------#
-usermod -s /usr/bin/zsh $SUDO_USER
-source ~/anaconda3/etc/profile.d/conda.sh
-conda init zsh
-/bin/echo -e "\e[1;36m   !----------------------------------------------------------------!\e[0m"
-/bin/echo -e "\e[1;36m   !  Installation Completed...Please test your ZSH configuration   !\e[0m" 
-/bin/echo -e "\e[1;36m   !          now close your terminal and open it again             !\e[0m"
-/bin/echo -e "\e[1;36m   !      and change defult font from Properties>Fonts to the       !\e[0m"
-/bin/echo -e "\e[1;36m                 [ DejaVu Sans Mono Bold for Powerline ]            !\e[0m"
-/bin/echo -e "\e[1;36m   !                                                                !\e[0m"
-/bin/echo -e "\e[1;36m   !          Credits : Written by Siniorone - Aug. 2021            !\e[0m"
-/bin/echo -e "\e[1;36m   !          https://github.com/siniorone/WSL-Beautifier           !\e[0m"
-/bin/echo -e "\e[1;36m   !----------------------------------------------------------------!\e[0m"
