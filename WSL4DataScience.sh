8267213cba1d15bc8af50c97c127da979be36d9d
Sina Karaji <82142947+siniorone@users.noreply.github.com>
2021-12-27 11:27:34 +0330

    Update WSL4DataScience.sh

--- WSL4DataScience.sh
+++ WSL4DataScience.sh
@@ -232,6 +232,7 @@ mongodb_installation(){
     logger "MongoDB Installation"
     curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+    sudo apt update -y
     sudo apt install mongodb-org -y
     set -e
     if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
